41d43c9f0162f194a69850ff7292a6d368f624f9
Utkarsha Kshirsagar <utkarshakshirsagar4717@gmail.com>
2024-09-13 16:27:06 -0400

    made the code moduler

---
+++ src/components/Game.js
@@ -0,0 +1,97 @@
+import { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { calculateWinner } from "../utils/utils";
+import Board from "./Board";
+function Game() {
+  // const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+  const winner = calculateWinner(currentSquares);
+  let status;
+  const notify = (message) =>
+    toast(message, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "light",
+    });
+  useEffect(() => {
+    if (winner) {
+      notify("Winner: " + winner);
+    }
+  }, [winner]);
+  if (winner) {
+    status = "Winner : " + winner;
+  } else {
+    status = "Next Player : " + (xIsNext ? "X" : "O");
+  }
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to the move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li className="m-2" key={move}>
+        <button
+          type="button"
+          className="btn btn-info"
+          onClick={() => jumpTo(move)}
+        >
+          {description}
+        </button>
+      </li>
+    );
+  });
+
+  useEffect(() => {
+    notify("Welcome to Tic Tac Toe!");
+  }, []);
+
+  return (
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <div className="status">{status}</div>
+      <div className="game">
+        <div className="game-board">
+          <Board
+            xIsNext={xIsNext}
+            squares={currentSquares}
+            onPlay={handlePlay}
+          />
+        </div>
+        <div className="game-info ">
+          Steps:
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Game;
